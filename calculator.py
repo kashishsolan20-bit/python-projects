26079279adf6d191d229d6940315a283256f44e8
kashishsolan20-bit <kashishsolan20@gmail.com>
2026-03-09 02:07:05 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,14 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("1.Add  2.Subtract  3.Multiply  4.Divide")
+choice = input("Choose operation: ")
+
+if choice == "1":
+    print("Result:", a + b)
+elif choice == "2":
+    print("Result:", a - b)
+elif choice == "3":
+    print("Result:", a * b)
+elif choice == "4":
+    print("Result:", a / b)
